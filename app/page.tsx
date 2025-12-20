bb6059e99d8447db351310d9a91f313ff9ffcf8c
avan <100318118+avansear@users.noreply.github.com>
2025-12-19 19:47:53 -0500

    build error

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,4 @@
 import IntroText from "./components/intro-text";
-import { createClient } from '@/utils/supabase/server'
-import { cookies } from 'next/headers'
-
-export default async function Page() {
-  const cookieStore = await cookies()
-  const supabase = createClient(cookieStore)
-
-  const { data: todos } = await supabase.from('todos').select()
-
-  return (
-    <ul>
-      {todos?.map((todo) => (
-        <li>{todo}</li>
-      ))}
-    </ul>
-  )
-}
 
 export default function Page() {
   return (
