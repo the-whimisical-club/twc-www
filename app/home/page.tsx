7c31eb9f493938deccfc77735bece901fda16636
avan <100318118+avansear@users.noreply.github.com>
2025-12-21 00:26:14 -0500

    new home

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -7,8 +7,19 @@ export default async function HomePage() {
   return (
     <div className="bg-background min-h-screen flex flex-col items-center justify-center gap-8 p-8">
       <NavButtons username={user.email || 'user'} />
-      <div className="text-6xl text-foreground font-stack-sans-notch">
-        you're in
+      <div className="text-2xl md:text-4xl gap-8 text-foreground font-stack-sans-notch">
+        <p>you're in</p>
+        <p>ok, so things are a little different here at the whimsy club.</p>
+        <p>this place exists for people i think are cool. new members sign up through a form, and i decide weekly who we're letting in.</p>
+        <p>you probably don't know everyone here. and that's okay. i promise they're all kind, interesting, and worth knowing. talk to them. get curious. let your guard down a little.</p>
+        <p>we don't do profile pages. if you click on someone's profile picture, you'll be taken to their personal website instead. everyone tells their story differently, and we don't want to interfere with that.</p>
+        <p>if you don't have a website, we'll give you a simple profile page. but c'mon, it's probably time you make one.</p>
+        <p>you can share photos and thoughts here. thoughts are literally whatever's on your mind. whatever you feel like talking about.</p>
+        <p>like most social platforms, you can like posts and leave comments.</p>
+        <p>unlike most social platforms, you can't privately message anyone. everything here is public. every photo. every thought.</p>
+        <p>every thought shared here deserves respect. everyone is entitled to their own opinions.</p>
+        <p>and finally, for the sake of my bank account. there's a limit of 10 photo uploads per day. i don't own cloudflare yet, so yeah.</p>
+        <p>welcome :D</p>
       </div>
     </div>
   )
